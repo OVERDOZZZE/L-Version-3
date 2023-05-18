09406bb95406bf8fdba4419f13bef1eb2a599004
OVERDOZZZE <saparbekov113@gmail.com>
2023-05-19 02:30:20 +0600

    Commit

---
+++ Library/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("", all_books, name='all_books'),
+    path("detail_book/<int:id>/", book_info, name='book_detail'),
+    path("add_book/", new_book),
+    path("detail_book/<int:id>/update/", alter_book, name='update_book'),
+    path("add_author/", new_author),
+    path("add_publisher/", new_publisher),
+    path('login/', login_func, name='login')
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ Library/views.py
@@ -0,0 +1,93 @@
+from django.contrib.auth import authenticate, login
+from django.core.paginator import Paginator
+from django.http import HttpResponse
+
+from django.shortcuts import render, redirect
+from .models import Book, Author, Publisher
+from .forms import AddPublisherForm, AddBookForm, AddAuthorForm
+
+
+def all_books(request):
+    book_list = Book.objects.all()
+    paginator = Paginator(book_list, 5)
+    page_number = request.GET.get('page')
+    page_object = paginator.get_page(page_number)
+    context = {'books': page_object}
+
+    return render(request, 'all_books.html', context=context)
+
+
+def book_info(request, pk):
+    book = Book.objects.get(id=pk)
+    formatted_date = book.publication_date.strftime("%Y-%m-%d")
+    authors = Author.objects.all()
+    publishers = Publisher.objects.all()
+    context = {'book': book,
+               'authors': authors,
+               'publishers': publishers,
+               'date': formatted_date
+               }
+    return render(request, 'book_info.html', context=context)
+
+
+def new_book(request):
+    if request.method == 'POST':
+        form = AddBookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('all_books')
+    else:
+        form = AddBookForm()
+    return render(request, 'add_new.html', {'form': form})
+
+
+def new_author(request):
+    if request.method == 'POST':
+        form = AddAuthorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('all_books')
+    else:
+        form = AddAuthorForm()
+    return render(request, 'add_new.html', {'form': form})
+
+
+def new_publisher(request):
+    if request.method == 'POST':
+        form = AddPublisherForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('all_books')
+    else:
+        form = AddPublisherForm()
+    return render(request, 'add_new.html', {'form': form})
+
+
+def login_func(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('all_books')
+        else:
+            return HttpResponse('Wrong password or login')
+
+    return render(request, 'login.html')
+
+
+def alter_book(request, id):
+    book = Book.objects.get(id=id)
+
+    if request.method == 'POST':
+        book.title = request.POST.get('title')
+        book.author = Author.objects.get(id=request.POST.get('author_id'))
+        book.publisher = Publisher.objects.get(id=request.POST.get('publisher_id'))
+        book.publication_date = request.POST.get('publication_date')
+        if request.POST.get('cover_photo'):
+            book.cover_photo = '{}{}'.format('media/', request.POST.get('cover_photo'))
+        book.save()
+
+    return redirect('book_detail', id=id)
